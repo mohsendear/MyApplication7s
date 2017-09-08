9af36973474abd3bbd590ecb813dbc5e45c84ef4
Mohse <mohsenfarajpourr@gmail.com>
2017-09-09 03:59:22 +0430

    Initial commit

---
+++ app/src/main/java/com/mohsen/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mohsen.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sum(){
+        int x = 3+2;
+    }
+}
